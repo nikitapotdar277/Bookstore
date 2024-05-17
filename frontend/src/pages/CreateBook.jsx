c1069e753dfcf7956356492d702c52bd02057942
nikita <nikita.potdar15@gmail.com>
2024-05-17 16:38:16 -0400

    Edit books

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -17,15 +17,15 @@ const CreateBook = () => {
         };
         setLoading(true)
         axios
-        .post('http://localhost:5555/books', newBook)
-        .then(() => {
-            setLoading(false);
-            navigate('/');
-        })
-        .catch((error) => {
-            alert("An error occurred. Book not created!");
-            console.log(error);
-        })
+            .post('http://localhost:5555/books', newBook)
+            .then(() => {
+                setLoading(false);
+                navigate('/');
+            })
+            .catch((error) => {
+                alert("An error occurred. Book not created!");
+                console.log(error);
+            })
     };
 
     return (
@@ -38,46 +38,46 @@ const CreateBook = () => {
                         <div className="sm:col-span-2">
                             <label htmlFor="title" className="block text-sm font-semibold leading-6 text-gray-900">Book Title</label>
                             <div className="mt-2.5">
-                                <input 
-                                type="text" 
-                                name="title" 
-                                id="title"
-                                value = {title}
-                                onChange={(e) => setTitle(e.target.value)} 
-                                autoComplete="title" 
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <input
+                                    type="text"
+                                    name="title"
+                                    id="title"
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    autoComplete="title"
+                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                             </div>
                         </div>
                         <div className="sm:col-span-2">
                             <label htmlFor="author" className="block text-sm font-semibold leading-6 text-gray-900">Book Author</label>
                             <div className="mt-2.5">
-                                <input 
-                                type="text" 
-                                name="author" 
-                                id="author"
-                                value = {author}
-                                onChange={(e) => setAuthor(e.target.value)} 
-                                autoComplete="author" 
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <input
+                                    type="text"
+                                    name="author"
+                                    id="author"
+                                    value={author}
+                                    onChange={(e) => setAuthor(e.target.value)}
+                                    autoComplete="author"
+                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                             </div>
                         </div>
                         <div className="sm:col-span-2">
                             <label htmlFor="publishYear" className="block text-sm font-semibold leading-6 text-gray-900">Year Published</label>
                             <div className="mt-2.5">
-                                <input 
-                                type="number" 
-                                name="publishYear" 
-                                id="publishYear"
-                                value = {publishYear}
-                                onChange={(e) => setPublishYear(e.target.value)}  
-                                autoComplete="publishYear" 
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <input
+                                    type="number"
+                                    name="publishYear"
+                                    id="publishYear"
+                                    value={publishYear}
+                                    onChange={(e) => setPublishYear(e.target.value)}
+                                    autoComplete="publishYear"
+                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                             </div>
                         </div>
                         <div className="mt-10">
-                            <button 
-                            onClick={handleSaveBook} 
-                            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create Book</button>
+                            <button
+                                onClick={handleSaveBook}
+                                className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create Book</button>
                         </div>
                     </div>
                 </div>
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,8 +1,109 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import BackButton from '../components/BackButton';
 
 const EditBook = () => {
+  const [loading, setLoading] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [author, setAuthor] = useState([]);
+  const [publishYear, setPublishYear] = useState([]);
+  const [book, setBook] = useState([]);
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:5555/books/${id}`)
+    .then((response) => {
+      setTitle(response.data.title);
+      setAuthor(response.data.author);
+      setPublishYear(response.data.publishYear);
+      console.log(book)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }, [])
+
+  const handleEditBook = () => {
+
+    setLoading(true);
+    const bookToEdit = {
+      title,
+      author,
+      publishYear,
+    }
+
+    console.log(bookToEdit)
+
+    axios
+      .put(`http://localhost:5555/books/${id}`, bookToEdit)
+      .then(() => {
+        setLoading(false)
+        navigate('/')
+      })
+      .catch((error) => {
+        alert("Error updating the book details!");
+        console.log(error);
+      })
+
+  }
+
   return (
-    <div>EditBook</div>
+    <div className='p-1'>
+      <div className='isolate bg-white px-10 py-10'>
+        <BackButton />
+        <h1 className='text-3xl my-2'>Edit A Book</h1>
+        <div className="mx-auto mt-16 max-w-xl sm:mt-20">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div className="sm:col-span-2">
+              <label htmlFor="title" className="block text-sm font-semibold leading-6 text-gray-900">Book Title</label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  autoComplete="title"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="author" className="block text-sm font-semibold leading-6 text-gray-900">Book Author</label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="author"
+                  id="author"
+                  value={author}
+                  onChange={(e) => setAuthor(e.target.value)}
+                  autoComplete="author"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="publishYear" className="block text-sm font-semibold leading-6 text-gray-900">Year Published</label>
+              <div className="mt-2.5">
+                <input
+                  type="number"
+                  name="publishYear"
+                  id="publishYear"
+                  value={publishYear}
+                  onChange={(e) => setPublishYear(e.target.value)}
+                  autoComplete="publishYear"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              </div>
+            </div>
+            <div className="mt-10">
+              <button
+                onClick={handleEditBook}
+                className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Edit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
